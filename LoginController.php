65faf77a0b86dbcf254d05229730d255475060bc
Md. Mohsin Patwary <patwarydk@gmail.com>
2017-02-27 15:49:19 +0600

    Authentication file

--- LoginController.php
+++ LoginController.php
@@ -1,50 +1,39 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
-use Illuminate\Http\Request;
-use App\Http\Requests;
-use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
-class LoginController extends Controller {
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
 
-   public function index() {
-      return view("frontend/login");
-   }
+    use AuthenticatesUsers;
 
-   public function check(Request $request) {
-      if ($_POST['type'] == 1) {
-         $data = DB::table("student")->where("email", $_POST['email'])->where("password", $_POST['pass'])->paginate();
-         foreach ($data as $d) {
-            $request->session()->put("myid", $d->id);
-            $request->session()->put("mytype", 's');
-            $request->session()->put("mypicture", $d->picture);
-            $request->session()->put("myname", $d->name);
-            return redirect('/profile-management/view')->with('message', 'Save Successful');
-         }
-      } else if ($_POST['type'] == 2) {
-         $data = DB::table("teacher")->where("email", $_POST['email'])->where("password", $_POST['pass'])->paginate();
-         foreach ($data as $d) {
-            $request->session()->put("myid", $d->id);
-            $request->session()->put("mypicture", $d->picture);
-            $request->session()->put("mytype", 't');
-            return redirect('/profile-management/view')->with('message', 'Save Successful');
-         }
-      } else if ($_POST['type'] == 3) {
-         $data = DB::table("administrator")->where("email", $_POST['email'])->where("password", $_POST['pass'])->paginate();
-         foreach ($data as $d) {
-            $request->session()->put("myid", $d->id);
-            $request->session()->put("mypicture", $d->picture);
-            $request->session()->put("mytype", 'a');
-            return redirect('/profile-management/view')->with('message', 'Save Successful');
-         }
-      }
-      return redirect('/login')->with('message', 'Invalid Email or Password');
-   }
-   
-   public function logout(Request $request){
-      $request->session()->flush();
-      return redirect('/');
-   }
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
 
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest', ['except' => 'logout']);
+    }
 }
